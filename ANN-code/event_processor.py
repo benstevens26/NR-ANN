e70a785ab3bc5ac8ddcfb40a1c812f933320b7b2
Ben <benstevens2609@gmail.com>
2025-01-13 11:25:57 +0000

    fixed fup

--- ANN-code/event_processor.py
+++ ANN-code/event_processor.py
@@ -130,7 +130,7 @@ def extract_features(event, num_segments=15):
         Array of features including [name, length, energy, max_den, recoil_angle].
     """
     axis, mean_x, mean_y = event.get_principal_axis()
-    if axis is 'fup':
+    if axis == 'fup':
         return 'fup'
 
     recoil_angle = event.get_recoil_angle()
@@ -225,7 +225,7 @@ def event_processor(
             # event = noise_remover(event)
             event = smooth_operator(event)
             features = extract_features(event, num_segments=num_bisector_segments)
-            if features is 'fup':
+            if features == 'fup':
                 continue
             chunk.append(features)
 
