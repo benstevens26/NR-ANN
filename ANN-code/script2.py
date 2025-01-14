5be7a5056ecd857de66ab1b5fbc4e8514b6744d9
Ben <benstevens2609@gmail.com>
2025-01-14 09:04:37 +0000

    script2 amended for im1 feature analysis

--- ANN-code/script2.py
+++ ANN-code/script2.py
@@ -1,10 +1,10 @@
 from event_processor import event_processor, yield_events
 
-base_dirs = ["/vols/lz/tmarley/GEM_ITO/run/im0"]
+base_dirs = ["/vols/lz/tmarley/GEM_ITO/run/im1/C", "/vols/lz/tmarley/GEM_ITO/run/im1/F"]
 dark_dir = "/vols/lz/MIGDAL/sim_ims/darks"
 
 chunk_size = 30
-output_csv = "features_im0.csv"
+output_csv = "features_im1.csv"
 
 events = yield_events(base_dirs)
 
