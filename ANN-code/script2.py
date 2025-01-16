7f292aba636a76084ec9a4c2e69d9a1ccc35b7f2
Ben <benstevens2609@gmail.com>
2025-01-16 11:56:37 +0000

    train_CoNNCR.py test

--- ANN-code/script2.py
+++ ANN-code/script2.py
@@ -1,10 +1,10 @@
 from event_processor import event_processor, yield_events
 
-base_dirs = ["/vols/lz/tmarley/GEM_ITO/run/im4"]
+base_dirs = ["/vols/lz/tmarley/GEM_ITO/run/im3"]
 dark_dir = "/vols/lz/MIGDAL/sim_ims/darks"
 
 chunk_size = 30
-output_csv = "outputs/features_im4.csv"
+output_csv = "output/features_im3.csv"
 
 events = yield_events(base_dirs)
 
