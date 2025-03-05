92ce0b5fc3404a697bdf13d6f7c17a0e0d5e6ac8
Ben <benstevens2609@gmail.com>
2025-03-05 12:10:30 +0000

    add LENRI_CF4_2, LENRI_Ar_CF4_2

--- ANN-code/models.py
+++ ANN-code/models.py
@@ -8,10 +8,10 @@ class LENRI_CF4_1(nn.Module):
     """
     LENRI hyperparameter tuned model for CF4-1 dataset.
     """
-    def __init__(self, input_size=9):
+    def __init__(self):
         super(LENRI_CF4_1, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_size, 96),
+            nn.Linear(9, 96),
             nn.LeakyReLU(negative_slope=0.01),
             nn.Dropout(p=0.0864),
             nn.Linear(96, 62),
@@ -28,10 +28,10 @@ class LENRI_Ar_CF4_1(nn.Module):
     """
     LENRI hyperparameter tuned model for Ar-CF4-1 dataset.
     """
-    def __init__(self, input_size=9):
+    def __init__(self):
         super(LENRI_Ar_CF4_1, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_size, 117),
+            nn.Linear(9, 117),
             nn.LeakyReLU(negative_slope=0.01),
             nn.Dropout(p=0.0693),
             nn.Linear(117, 40),
@@ -46,3 +46,49 @@ class LENRI_Ar_CF4_1(nn.Module):
     def forward(self, x):
         return self.model(x)
     
+    
+class LENRI_CF4_2(nn.Module):
+    """
+    LENRI hyperparameter tuned model for CF4-2 dataset with optimized hyperparameters.
+    """
+    def __init__(self):
+        super(LENRI_CF4_2, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(18, 64),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(p=0.09),
+            nn.Linear(64, 48),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(p=0.09),
+            nn.Linear(48, 32),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(p=0.09),
+            nn.Linear(32, 2),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+    
+
+class LENRI_Ar_CF4_2(nn.Module):
+    """
+    LENRI hyperparameter tuned model for Ar-CF4-2 dataset.
+    """
+    def __init__(self):
+        super(LENRI_Ar_CF4_2, self).__init__()
+        hidden_layers = [64, 48, 32]
+        self.model = nn.Sequential(
+            nn.Linear(18, hidden_layers[0]),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(p=0.08408783486673037),
+            nn.Linear(hidden_layers[0], hidden_layers[1]),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(p=0.08408783486673037),
+            nn.Linear(hidden_layers[1], hidden_layers[2]),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(p=0.08408783486673037),
+            nn.Linear(hidden_layers[2], 3),
+        )
+
+    def forward(self, x):
+        return self.model(x)
