c5ce46aca716a4fb0d4bd2d531c20dea8f54d89a
Ben <benstevens2609@gmail.com>
2025-03-05 12:08:44 +0000

    add class Event3D

--- ANN-code/bb_event.py
+++ ANN-code/bb_event.py
@@ -144,3 +144,37 @@ def load_events_bb(file_path):
             events.append(event)
 
     return events
+
+
+class Event3D:
+    """
+    A class to represent a 3D event and hold its attributes.
+
+    Attributes:
+    ----------
+    name : str
+        The filename of the event.
+    image : np.ndarray
+        2D array representing the image data of the event.
+    species : str
+        The species ('C' for carbon or 'F' for fluorine) extracted from the filename.
+    """
+
+    def __init__(self, cam_path, cam_image, ito_path, ito_image):
+        """
+        Initialize an Event instance.
+
+        Parameters:
+        ----------
+        name : str
+            The filename of the event.
+        image : np.ndarray
+            2D array representing the image data of the event, either raw or processed.
+        """
+
+        self.cam_path = cam_path
+        self.cam_image = cam_image
+        self.ito_path = ito_path
+        self.ito_image = ito_image
+
+ 
